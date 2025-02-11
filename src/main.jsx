bd2706328e308045ec7824e047f7849f708d9011
ilathem <isaiahlathem@gmail.com>
2025-02-10 20:27:02 -0600

    implement error page

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import Root from './routes/root';
+import ErrorPage from './error-page';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Root /> 
-  }
-])
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
