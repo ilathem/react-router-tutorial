d7bc6508f489a435c74fd09bd88c498f3d5f299a
ilathem <isaiahlathem@gmail.com>
2025-02-13 19:39:50 -0600

    convert routes to jsx
    the tutorial concludes with converting the router object
    to jsx to show how that compares, since it is generally
    more popular to use jsx than javascript object notation

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from 'react-router-dom';
 import './index.css';
 import Root, {
   loader as rootLoader,
@@ -12,46 +17,75 @@ import Contact, {
   action as contactAction,
 } from './routes/contact';
 import EditContact, { action as editAction } from './routes/edit';
-import { action as destoryAction } from './routes/destroy';
+import { action as destroyAction } from './routes/destroy';
 import Index from './routes';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            index: true,
-            element: <Index />,
-          },
-          {
-            path: 'contacts/:contactId',
-            element: <Contact />,
-            loader: contactLoader,
-            action: contactAction,
-          },
-          {
-            path: 'contacts/:contactId/edit',
-            element: <EditContact />,
-            loader: contactLoader,
-            action: editAction,
-          },
-          {
-            path: 'contacts/:contactId/destroy',
-            action: destoryAction,
-            errorElement: <div>Oops! There was an error.</div>,
-          },
-        ],
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Root />,
+//     errorElement: <ErrorPage />,
+//     loader: rootLoader,
+//     action: rootAction,
+//     children: [
+//       {
+//         errorElement: <ErrorPage />,
+//         children: [
+//           {
+//             index: true,
+//             element: <Index />,
+//           },
+//           {
+//             path: 'contacts/:contactId',
+//             element: <Contact />,
+//             loader: contactLoader,
+//             action: contactAction,
+//           },
+//           {
+//             path: 'contacts/:contactId/edit',
+//             element: <EditContact />,
+//             loader: contactLoader,
+//             action: editAction,
+//           },
+//           {
+//             path: 'contacts/:contactId/destroy',
+//             action: destoryAction,
+//             errorElement: <div>Oops! There was an error.</div>,
+//           },
+//         ],
+//       },
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path='/'
+      element={<Root />}
+      loader={rootLoader}
+      action={rootAction}
+      errorElement={<ErrorPage />}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />} />
+        <Route
+          path='contacts/:contactId'
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+        <Route
+          path='contacts/:contactId/edit'
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editAction}
+        />
+        <Route path='contacts/:contactId/destroy' action={destroyAction} />
+      </Route>
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
